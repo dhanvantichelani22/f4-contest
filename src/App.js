839e63a2b461669b92998221e6b6db9d9c34778c
dhanvantichelani <dhanvantichelani865@gmail.com>
2023-08-26 23:45:26 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+       <input type='text' placeholder='search'/>&nbsp;&nbsp;&nbsp;
+       <button>Search</button>
     </div>
   );
 }
